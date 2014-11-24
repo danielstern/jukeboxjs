4a9c69ac13002dd8c34efca196c46b76e660f02b
Daniel Stern <daniel@danielstern.ca>
2014-11-23 19:14:52 -0500

    moved-sequence-to-timer-family

--- app/scripts/jukebox.js
+++ app/scripts/jukebox.js
@@ -3,6 +3,7 @@ var jukeboxTimer = function(){
   var lasttime = new Date().getTime();
   var framebuffer = 0;
   var framespassed = 0;
+  var timer = this;
 
   setInterval(function(){
     var frametime = new Date().getTime();
@@ -29,24 +30,40 @@ var jukeboxTimer = function(){
     return interval;
   }
 
+  this.setSequence = function(actions) {
+    
+    var sequenceQueuedActions = [];
+    var timeSoFar = 0;
+    actions.forEach(function(action){
+      sequenceQueuedActions.push(timer.setTimeout(function(){
+        action.callback()
+      },timeSoFar));
+      timeSoFar += action.timeout;
+    });
+
+    return sequenceQueuedActions;
+  }
+
+  this.clearSequence = function(sequenceQueuedActions) {
+    sequenceQueuedActions.forEach(function(action,index){
+      clearTimeout(action);
+    });
+  }
+
   this.setTimeout = function(callback,timeout,arguments) {
     var startframe = framespassed;
     var interval = setInterval(function(){
       var totaltimepassed = framespassed - startframe;
-      // console.log("Total time passed?",totaltimepassed);
       if (totaltimepassed >= timeout) {
-        console.log("Interval cleared");
         callback(arguments);
         clearInterval(interval);
       }
-      // startframe = framespassed;
     },1);
     
     return interval;
   }
 
   function trigger() {
-    // console.log("A frame has passed");
     framespassed+=5;
   }
 }
@@ -146,20 +163,22 @@ var Jukebox = {
     oscillators.push(osc);
 		oscillators.push(osc2);
 
-    var sequenceQueuedActions = [];
+   
 
 		this.sequence = function(notes) {
-      sequenceQueuedActions.forEach(function(action,index){
-        clearTimeout(action);
-      });
+      var sequence = Jukebox.timer.setSequence(notes.map(function(note){
+        return {
+          timeout: note.duration,
+          callback: function() {
+            synth.tone(note.frequency,note.duration-10);  
+          }
+        }
+      }))
 
-			var durationSoFar = 0;
-			notes.forEach(function(element){
-				sequenceQueuedActions.push(Jukebox.timer.setTimeout(function(){
-					synth.tone(element.frequency,element.duration-10);
-				},durationSoFar));
-				durationSoFar += element.duration;
-			});
+      // Jukebox.timer.clearSequence(sequence);
+
+
+			
 		}
 
 		this.tone = function(freq,duration) {
