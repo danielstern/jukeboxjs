b617b239f169c6c224c403f3925546a513a05305
Daniel Stern <daniel@danielstern.ca>
2014-12-04 11:59:23 -0500

    Updating whole frameworks. synthesizer is gross mockery of what it was in terms of sounds but works. framework does not seems optimized for reusing oscillators.

--- app/scripts/jukebox.js
+++ app/scripts/jukebox.js
@@ -1,144 +1,169 @@
 var Jukebox = function() {
-  if (window.jukeboxAudioContext) {
-    audioContext = jukeboxAudioContext;
-  } else {
-	 var audioContext = webkitAudioContext ? new webkitAudioContext() : null; 
-   window.jukeboxAudioContext = audioContext;
-  }
-  var timer = new jukeboxTimer();
-  var filter = new webAudioFilterPack(this.audioContext);
+    if (window.jukeboxAudioContext) {
+        audioContext = jukeboxAudioContext;
+    } else {
+        var audioContext = webkitAudioContext ? new webkitAudioContext() : null;
+        window.jukeboxAudioContext = audioContext;
+    }
+    var timer = new jukeboxTimer();
+    var filter = new webAudioFilterPack(this.audioContext);
 
-  var Oscillator = function(options) {
+    var Modulator = function(options) {
+        options = options || {};
+
+        options.oscillators = ["SINE", "SQUARE"];
+        options.effects = [];
+        options.frequency = 440;
+        options.noteLength = 300;
+
         var context = audioContext,
-        oscillator = this,
-        _target,
-        _oscillatorRunning = false,
-        frequency = options.frequency || 440,
-        type = options.type || "SINE";
-
-        var _oscillator = context.createOscillator();
-        var gain = audioContext.createGain();
+            targetAudioNode,
+            oscillators = [],
+            playing = false,
+            gain,
+            frequency = options.frequency,
+            type = options.type || "SINE";
+        gain = audioContext.createGain();
         gain.connect(audioContext.destination);
 
+        oscillators = options.oscillators.map(function(oscillatorDefinition) {
+            var oscillator = context.createOscillator();
+            oscillator.type = oscillatorDefinition;
+            oscillator.connect(gain);
+            oscillator.noteOn(1);
+            oscillator.frequency.value = 0;
+            return oscillator;
+        })
 
         var play = function() {
-          if (_oscillatorRunning) return;
-
-          _oscillator = context.createOscillator(); // Create sound source
-
-          if (_target) {
-           _oscillator.connect(_target); // Connect sound to output
-           _target.connect(gain); // Connect sound to output
-          } else {
-           _oscillator.connect(gain); // Connect sound to output 
-          }
-
-          _oscillator.frequency.value = frequency;
-          _oscillator.type = type;
-          _oscillator.noteOn(1);
-          _oscillatorRunning = true;
+            playing = true;
+            updateOscillators();
         }
 
         var stop = function() {
-          if (!_oscillatorRunning) return;
-          _oscillator.noteOff(1);
-          _oscillatorRunning = false;
+            playing = false;
+            updateOscillators();
         }
 
-        var setFrequency = function(_frequency){
-          frequency = _frequency;
-          _oscillator.frequency.value = _frequency;
+        var updateOscillators = function() {
+            oscillators.forEach(function(oscillator) {
+                // var oscillator = context.createOscillator();
+                // oscillator.type = oscillatorDefinition;
+
+                if (targetAudioNode) {
+                    oscillator.connect(targetAudioNode); // Connect sound to output
+                    targetAudioNode.connect(gain); // Connect sound to output
+                } else {
+                    oscillator.connect(gain); // Connect sound to output 
+                }
+
+                oscillator.type = type;
+                if (playing) {
+                    oscillator.frequency.value = frequency;
+                } else {
+                    oscillator.frequency.value = 0;
+                }
+            });
         }
 
-        var setType = function(_wave) {
-          type = wave;
-          _oscillator.type = _oscillator[wave];
+        var setFrequency = function(_frequency) {
+            frequency = _frequency;
+            updateOscillators();
         }
+
+        // var setType = function(type) {
+        //   type = wave;
+        //   _oscillator.type = _oscillator[wave];
+        // }
 
         this.connect = function(node) {
-          _target = node;
+            targetAudioNode = node;
         }
 
         return {
-          play:play,
-          stop:stop,
-          setFrequency:setFrequency,
-          gain:gain
+            play: play,
+            stop: stop,
+            setFrequency: setFrequency,
+            gain: gain
         }
     };
 
-	var Synthesizer = function(options) {
-    options = options || {};
-    var oscillators,
-    currentSequence;
+    var Synthesizer = function(options) {
+        options = options || {};
+        var oscillators,
+            currentSequence;
 
-    if (options.oscillators) {
-      oscillators = options.oscillators.map(function(schema){
-        return new Oscillator(schema);
-      });
-    } else {
-      oscillators = [new Oscillator({wave:"SINE"}),new Oscillator({wave:"SQUARE"})];
-    }
-
-		var sequence = function(notes) {
-      endSequence();
-      currentSequence = timer.setSequence(notes.map(function(note){
-        return {
-          timeout: note.duration,
-          callback: function() {
-            synth.tone(note.frequency,note.duration-10);  
-          }
+        if (options.oscillators) {
+            oscillators = options.oscillators.map(function(schema) {
+                return new Modulator({
+                    oscillators: ["SINE", "SQUARE"]
+                });
+            });
+        } else {
+            oscillators = [new Modulator({
+                oscillators: ["SINE", "SQUARE"]
+            })];
         }
-      }))
-		};
 
-    var setVolume = function(volume) {
-      oscillators.forEach(function(oscillator){
-        oscillator.gain.value = volume;
-      })
+        var sequence = function(notes) {
+            endSequence();
+            currentSequence = timer.setSequence(notes.map(function(note) {
+                return {
+                    timeout: note.duration,
+                    callback: function() {
+                        synth.tone(note.frequency, note.duration - 10);
+                    }
+                }
+            }))
+        };
+
+        var setVolume = function(volume) {
+            oscillators.forEach(function(oscillator) {
+                oscillator.gain.value = volume;
+            })
+        }
+
+        var endSequence = function() {
+            if (currentSequence) {
+                timer.clearSequence(currentSequence);
+            }
+        }
+
+        var tone = function(freq, duration) {
+            oscillators.forEach(function(osc) {
+                osc.setFrequency(freq);
+                if (freq < 0) {
+                    return;
+                }
+                osc.play();
+                timer.setTimeout(function() {
+                    osc.stop();
+                }, duration);
+            })
+        };
+
+        return {
+            setVolume: setVolume,
+            tone: tone,
+            sequence: sequence,
+        }
+    };
+
+    var Patterns = {
+
     }
 
-    var endSequence = function() {
-      if (currentSequence) {
-        timer.clearSequence(currentSequence);
-      }
-    }
 
-		var tone = function(freq,duration) {
-			oscillators.forEach(function(osc){
-				osc.setFrequency(freq);
-				if (freq < 0) {
-					return;
-				}
-				osc.play();
-				timer.setTimeout(function(){
-					osc.stop();
-				},duration);
-			})
-		};
 
     return {
-      setVolume:setVolume,
-      tone:tone,
-      sequence:sequence,
+        getSynth: function(options) {
+            return new Synthesizer(options);
+        },
+        getModulator: function(options) {
+            return new Modulator(options);
+        },
+        getContext: function() {
+            return audioContext;
+        }
     }
-	};
-
-  var Patterns = {
-
-  }
-
-  
-
-  return {
-    getSynth:function(options){
-      return new Synthesizer(options);
-    },
-    getModulator:function(options){
-      return new Oscillator(options);
-    },
-    getContext:function(){
-      return audioContext;
-    }
-  }
 }
