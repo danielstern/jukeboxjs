ae5c7ffa2457ecda501203fd2053cd708b9d3038
Daniel Stern <daniel@danielstern.ca>
2014-12-04 00:30:53 -0500

    simplified oscillator class.

--- app/scripts/jukebox.js
+++ app/scripts/jukebox.js
@@ -1,104 +1,90 @@
 var Jukebox = function() {
   var jukebox = this;
-	this.audioContext =  new webkitAudioContext();
-  this.timer = new jukeboxTimer();
-  this.filter = new webAudioFilterPack(this.audioContext);
+	var audioContext = webkitAudioContext ? new webkitAudioContext() : null;
+  var timer = new jukeboxTimer();
+  var filter = new webAudioFilterPack(this.audioContext);
 
   window.addEventListener("click",function twiddle(){
-    var _oscillator = jukebox.audioContext.createOscillator();
+    var _oscillator = audioContext.createOscillator();
     _oscillator.noteOn(0.1);
     window.removeEventListener("click",twiddle);
-  })
+  });
 
-  this.gainNode = function(options) {
-      var gain = jukebox.audioContext.createGain();
-      gain.connect(jukebox.audioContext.destination);
-      gain.gain.value = 0.5;
-      return gain;
-  };
 
-  this.oscillator = function(options) {
-        var context = jukebox.audioContext;
-        var oscillator = this;
-        var target = undefined;
+  var Oscillator = function(options) {
+        var context = audioContext,
+        oscillator = this,
+        _target,
+        _oscillatorRunning = false,
+        frequency = options.frequency || 440,
+        type = options.type || "SINE";
+
         var _oscillator = context.createOscillator();
+        var gain = audioContext.createGain();
+        gain.connect(audioContext.destination);
 
-        this.gain = new jukebox.gainNode();
-        this.frequency = options.frequency;
 
-        var _oscillatorRunning = false;
+        var play = function() {
+          if (_oscillatorRunning) return;
 
-        this.wave = options.wave || "SINE";
+          _oscillator = context.createOscillator(); // Create sound source
 
-        this.start = function() {
-            this.playing = true;
+          if (_target) {
+           _oscillator.connect(_target); // Connect sound to output
+           _target.connect(gain); // Connect sound to output
+          } else {
+           _oscillator.connect(gain); // Connect sound to output 
+          }
+
+          _oscillator.frequency.value = frequency;
+          _oscillator.type = type;
+          _oscillator.noteOn(1);
+          _oscillatorRunning = true;
         }
 
-        this.stop = function() {
-            this.playing = false;
+        var stop = function() {
+          if (!_oscillatorRunning) return;
+          _oscillator.noteOff(1);
+          _oscillatorRunning = false;
+        }
+
+        var setFrequency = function(_frequency){
+          frequency = _frequency;
+          _oscillator.frequency.value = _frequency;
+        }
+
+        var setType = function(_wave) {
+          type = wave;
+          _oscillator.type = _oscillator[wave];
         }
 
         this.connect = function(node) {
-          target = node;
+          _target = node;
         }
 
-        setInterval(adjustvalues, 10);
-
-        function adjustvalues() {
-
-            if (_oscillator.frequency.value !== oscillator.frequency && oscillator.frequency) {
-                _oscillator.frequency.value = oscillator.frequency;
-            }
-
-            if (oscillator.playing) {
-                oscillator._startTone();
-            } else {
-                oscillator._stopTone();
-            }
-
-            _oscillator.type = _oscillator[oscillator.wave];
-        };
-
-
-        this._startTone = function() {
-           if (_oscillatorRunning) return;
-           _oscillator = context.createOscillator(); // Create sound source
-           if (target) {
-            _oscillator.connect(target); // Connect sound to output
-            target.connect(oscillator.gain); // Connect sound to output
-           } else {
-            _oscillator.connect(oscillator.gain); // Connect sound to output 
-           }
-           _oscillator.connect(oscillator.gain); // Connect sound to output
-           _oscillator.frequency.value = oscillator.frequency;
-           _oscillator.noteOn(1);
-           _oscillatorRunning = true;
-        };
-
-        this._stopTone = function() {
-            if (!_oscillatorRunning) return;
-            _oscillator.noteOff(1);
-            _oscillatorRunning = false;
+        return {
+          play:play,
+          stop:stop,
+          setFrequency:setFrequency,
+          gain:gain
         }
-
-        adjustvalues();
-    },
+    };
 	this.Synth = function(options) {
     options = options || {};
 		var synth = this;
     var oscillators;
     if (options.oscillators) {
       oscillators = options.oscillators.map(function(schema){
-        return new jukebox.oscillator(schema);
+        return new Oscillator(schema);
       });
     } else {
-		  oscillators = [new jukebox.oscillator({wave:"SINE"}),new jukebox.oscillator({wave:"SQUARE"})];
+		  oscillators = [new Oscillator({wave:"SINE"}),new Oscillator({wave:"SQUARE"})];
     }
 
     var currentSequence = undefined;
 		this.sequence = function(notes) {
       this.endSequence();
-      currentSequence = jukebox.timer.setSequence(notes.map(function(note){
+      currentSequence = timer.setSequence(notes.map(function(note){
         return {
           timeout: note.duration,
           callback: function() {
@@ -116,18 +102,18 @@ var Jukebox = function() {
 
     this.endSequence = function() {
       if (currentSequence) {
-        jukebox.timer.clearSequence(currentSequence);
+        timer.clearSequence(currentSequence);
       }
     }
 
 		this.tone = function(freq,duration) {
 			oscillators.forEach(function(osc){
-				osc.frequency = freq;
+				osc.setFrequency(freq);
 				if (freq < 0) {
 					return;
 				}
-				osc.start();
-				jukebox.timer.setTimeout(function(){
+				osc.play();
+				timer.setTimeout(function(){
 					osc.stop();
 				},duration);
 			})
@@ -136,20 +122,19 @@ var Jukebox = function() {
   this.Drums = function() {
     
     var drums = this;
-    var context = jukebox.audioContext;
+    var context = audioContext;
 
     var kickoscillators = [
-      new jukebox.oscillator({wave:"SINE"}),
-      new jukebox.oscillator({wave:"SINE"}),
-      // new jukebox.oscillator({wave:"TRIANGLE"}),
-      new jukebox.oscillator({wave:"SINE"})
+      new Oscillator({wave:"SINE"}),
+      new Oscillator({wave:"SINE"}),
+      new Oscillator({wave:"SINE"})
     ];
 
     this.kickdrum = function(){
       kickoscillators.forEach(function(osc){
         var freq = 40;
-        osc.frequency = freq;
-        osc.start();
+        osc.setFrequency(freq);
+        osc.play();
 
         osc.gain.gain.linearRampToValueAtTime(0, context.currentTime); // envelope  
         osc.gain.gain.linearRampToValueAtTime(1, context.currentTime + 0.01); // envelope  
@@ -158,11 +143,11 @@ var Jukebox = function() {
       })
     }
 
-    var cymbalOscillators = [new jukebox.oscillator({wave:"SINE",frequency:550}),new jukebox.oscillator({wave:"SINE",frequency:630}),new jukebox.oscillator({wave:"SINE",frequency:720})];
+    var cymbalOscillators = [new Oscillator({wave:"SINE",frequency:550}),new Oscillator({wave:"SINE",frequency:630}),new Oscillator({wave:"SINE",frequency:720})];
     this.cymbal = function () {
 
       cymbalOscillators.forEach(function(osc){
-        osc.start();
+        osc.play();
         osc.gain.gain.linearRampToValueAtTime(0, context.currentTime); // envelope  
         osc.gain.gain.linearRampToValueAtTime(0.3, context.currentTime + 0.03); // envelope  
         osc.gain.gain.linearRampToValueAtTime(0, context.currentTime + 0.5); // envelope  
@@ -170,15 +155,15 @@ var Jukebox = function() {
     }
 
     var hihatoscillators = [
-      new jukebox.oscillator({wave:"SINE",frequency:700}),
-      new jukebox.oscillator({wave:"SINE",frequency:720}),
-      new jukebox.oscillator({wave:"SQUARE",frequency:740})
+      new Oscillator({wave:"SINE",frequency:700}),
+      new Oscillator({wave:"SINE",frequency:720}),
+      new Oscillator({wave:"SQUARE",frequency:740})
     ];
     this.hihat = function () {
 
       hihatoscillators.forEach(function(osc){
 
-        osc.start();
+        osc.play();
         osc.gain.gain.linearRampToValueAtTime(0, context.currentTime); // envelope  
         osc.gain.gain.linearRampToValueAtTime(0.1, context.currentTime + 0.01); // envelope  
         osc.gain.gain.linearRampToValueAtTime(0, context.currentTime + 0.03); // envelope  
@@ -186,14 +171,14 @@ var Jukebox = function() {
     }
 
     var snareOscillators = [
-      new jukebox.oscillator({wave:"SINE",frequency:220}),
-      new jukebox.oscillator({wave:"SINE",frequency:270}),
-      new jukebox.oscillator({wave:"TRIANGLE",frequency:300})
+      new Oscillator({wave:"SINE",frequency:220}),
+      new Oscillator({wave:"SINE",frequency:270}),
+      new Oscillator({wave:"TRIANGLE",frequency:300})
     ];
     this.snare = function(){
 
       snareOscillators.forEach(function(osc){
-        osc.start();
+        osc.play();
 
         osc.gain.gain.linearRampToValueAtTime(0, context.currentTime); // envelope  
         osc.gain.gain.linearRampToValueAtTime(1, context.currentTime + 0.01); // envelope  
